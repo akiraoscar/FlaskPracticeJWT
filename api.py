62d0e0c5de20f8411167d6ba074abe4e776f8952
akira <akira@alterra.id>
2021-05-20 17:32:52 +0700

    mengganti jwt ke flask jwt extended

--- api.py
+++ api.py
@@ -2,13 +2,17 @@ from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 import uuid
 from werkzeug.security import generate_password_hash, check_password_hash
-import jwt
 import datetime
-from functools import wraps
+
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import get_jwt
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
-
-app.config['SECRET_KEY'] = 'akira'
+app.config["JWT_SECRET_KEY"] = "jkasirannfala"  # Change this!
+jwt = JWTManager(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 
 db = SQLAlchemy(app)
@@ -20,33 +24,10 @@ class User(db.Model):
     password = db.Column(db.String(80))
     admin = db.Column(db.Boolean)
 
-def token_required(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        token = None
-
-        if 'x-access-token' in request.headers:
-            token = request.headers['x-access-token']
-
-        if not token:
-            return jsonify({'message' : 'Token is missing!'}), 401
-
-        try: 
-            data = jwt.decode(token, app.config['SECRET_KEY'])
-            current_user = User.query.filter_by(public_id=data['public_id']).first()
-        except:
-            return jsonify({'message' : 'Token is invalid!'}), 401
-
-        return f(current_user, *args, **kwargs)
-
-    return decorated
-
-
 @app.route('/user', methods=['GET'])
-@token_required
-def get_all_users(current_user):
-
-    if not current_user.admin:
+@jwt_required()
+def get_all_users():
+    if not get_jwt()["admin"]:
         return jsonify({'message' : 'Cannot perform that function!'})
 
     users = User.query.all()
@@ -64,10 +45,10 @@ def get_all_users(current_user):
     return jsonify({'users' : output})
 
 @app.route('/user/<public_id>', methods=['GET'])
-@token_required
-def get_one_user(current_user, public_id):
+@jwt_required()
+def get_one_user(public_id):
 
-    if not current_user.admin:
+    if not get_jwt()["admin"]:
         return jsonify({'message' : 'Cannot perform that function!'})
 
     user = User.query.filter_by(public_id=public_id).first()
@@ -84,9 +65,10 @@ def get_one_user(current_user, public_id):
     return jsonify({'user' : user_data})
 
 @app.route('/user', methods=['POST'])
-def create_user(current_user):
+@jwt_required()
+def create_user():
 
-    if not current_user.admin:
+    if not get_jwt()["admin"]:
         return jsonify({'message' : 'Cannot perform that function!'})
 
     data = request.get_json()
@@ -100,9 +82,10 @@ def create_user(current_user):
     return jsonify({'message' : 'New user created!'})
 
 @app.route('/user/<public_id>', methods=['PUT'])
-def promote_user(current_user, public_id):
+@jwt_required()
+def promote_user(public_id):
 
-    if not current_user.admin:
+    if not get_jwt()["admin"]:
         return jsonify({'message' : 'Cannot perform that function!'})
 
     user = User.query.filter_by(public_id=public_id).first()
@@ -116,9 +99,10 @@ def promote_user(current_user, public_id):
     return jsonify({'message' : 'The user has been promoted!'})
 
 @app.route('/user/<public_id>', methods=['DELETE'])
-def delete_user(current_user, public_id):
+@jwt_required()
+def delete_user(public_id):
 
-    if not current_user.admin:
+    if not get_jwt()["admin"]:
         return jsonify({'message' : 'Cannot perform that function!'})
 
     user = User.query.filter_by(public_id=public_id).first()
@@ -144,9 +128,10 @@ def login():
         return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
 
     if check_password_hash(user.password, auth.password):
-        token = jwt.encode({'public_id' : user.public_id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
-
-        return jsonify({'token' : token.decode('UTF-8')})
+        expires = datetime.timedelta(days=1)
+        additional_claims = {"admin" : user.admin}
+        access_token = create_access_token(identity=auth.username, expires_delta=expires, additional_claims=additional_claims)
+        return jsonify(access_token=access_token)
 
     return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
 
